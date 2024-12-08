a19eb964e9f40b30809367c6d92684c90258312c
shantanu289 <shuklashantanu289@gmail.com>
2024-12-07 18:08:44 -0800

    MPC controller skeleton

--- control.py
+++ control.py
@@ -1,6 +1,8 @@
 import numpy as np
 import casadi as ca
 import pybullet as p
+from camera import *
+from utils import *
 
 class Control:
     def __init__(self, N, time_step, sim_time):
@@ -8,8 +10,18 @@ class Control:
         self.time_step = time_step # simulation time step
         self.sim_time = sim_time # current simulation time step
         self.ref_state = np.array([[0],[0]])
+        self.state_lower_bound = [...]
+        self.state_upper_bound = [...]
+        self.control_lower_bound = [...]
+        self.control_upper_bound = [...]
+        self.Q = np.array(...)
+        self.R = np.array(...)
+        self.tempCamera = CameraModule()
 
-    def motionModel(self, curr_, objectWorldCoords, U, current_ee_pose):
+    def getObjectCoords(self, object_id, i):
+        pass
+
+    def motionModel(self, objectWorldCoords, U, current_ee_pose):
         # curr_ -> current state of the object (u,v) pixels
         # objectWorldCoords -> (x,y,z) in the NEXT time step of the object in the world frame
         # U -> input given to camera (xdot, ydot, zdot, rolldot, pitchdot, yawdot) (np.array)
@@ -20,11 +32,16 @@ class Control:
         new_ee_pose = current_ee_pose + self.time_step*U    
 
         # calculate the pixel co-ordinates of the object given the positions of the the object and the camera
+        camera_position = tuple(new_ee_pose[:3])
+        camera_orientation = tuple(new_ee_pose[3:])
+        self.tempCamera.get_camera_view_and_projection_opencv(camera_position=camera_position, camera_orientation=camera_orientation)
+        pixel_coordinates, z_coordinate = self.tempCamera.opengl_plot_world_to_pixelspace(objectWorldCoords)
         
+        pixel_coordinates = np.array(pixel_coordinates)
+        pixel_coordinates.reshape(2,1)
+        return pixel_coordinates
 
-
-
-    def performControl(self, curr_state, curr_ref_state, current_ee_pose):
+    def performControl(self, current_ee_pose):
         # initialize stuff
         U = ca.MX.sym('U', 6, self.N)
         g = []     # has predicted states of the next N time steps
@@ -33,8 +50,7 @@ class Control:
         ubx = []   # upper bound of the control input for the next N time steps
         lbx = []   # lower bound of the control input for the next N time steps
         obj = 0    # objective function that will calculated over the next N time steps --> to minimize
-
-        curr_ = curr_state  # current state of the system
+        
 
         # set the casadi objects in loop        
         for i in range (self.N) :
@@ -43,7 +59,7 @@ class Control:
             objectWorldCoords = self.getObjectCoords(i)
 
             # get the next state given the control and the object location in world frame
-            predicted_state = self.motionModel(curr_, objectWorldCoords, np.array([U[0,i], U[1,i], U[2,i], U[3,i], U[4,i], U[5,i]]), current_ee_pose)
+            predicted_state = self.motionModel(objectWorldCoords, np.array([U[0,i], U[1,i], U[2,i], U[3,i], U[4,i], U[5,i]]), current_ee_pose)
 
             # calculate the error in state and add to the objective
             state_error = self.ref_state - predicted_state
@@ -52,9 +68,40 @@ class Control:
             # add the predicted state and the bounds to casadi objects
             g.append(predicted_state[0]); lbg.append(self.state_lower_bound[0]); ubg.append(self.state_upper_bound[0])
             g.append(predicted_state[1]); lbg.append(self.state_lower_bound[1]); ubg.append(self.state_upper_bound[1])
+            lbx.append(self.control_lower_bound[0]); ubx.append(self.control_upper_bound[0])
+            lbx.append(self.control_lower_bound[1]); ubx.append(self.control_upper_bound[1])
+            lbx.append(self.control_lower_bound[2]); ubx.append(self.control_upper_bound[2])
+            lbx.append(self.control_lower_bound[3]); ubx.append(self.control_upper_bound[3])
+            lbx.append(self.control_lower_bound[4]); ubx.append(self.control_upper_bound[4])
+            lbx.append(self.control_lower_bound[5]); ubx.append(self.control_upper_bound[5])
+        
+        opt_variables = ca.vertcat(ca.reshape(U,-1,1))
 
-            # update the current state to the current prediction to be used for the next step
-            curr_ = predicted_state
+        # initial guess for inputs and states
+        u0 = ...
+
+        # define optimization solver
+        nlp = {'f':obj, 'x':opt_variables, 'g':ca.vertcat(*g)}
+        solver = ca.nlpsol("S", "ipopt", nlp)
+        sol = solver(
+            x0= u0.T.reshape(-1,1),  # initial guess
+            lbx=lbx, # lower bound on optimization variables
+            ubx=ubx, # upper bound on optimization variables
+            lbg=lbg, # lower bound on optimization constraints
+            ubg=ubg, # upper bound on optimization constraints
+        )
+
+        # get the solution
+        x = sol["x"] 
+
+        # extract the control input and return
+        x = x.full()
+        u = x[:6]
+        return u
+    
+if __name__ == "__main__":
+    pass
+
 
 
         
